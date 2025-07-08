eddcf080b092f9dc8c1b62cc7beee12c5c53185d
Dae Dowdell <dow_dell2010@yahoo.com>
2025-07-08 11:34:04 -0400

    setting up html

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
 /*-------------- Constants -------------*/
-
+const maxPlayers = 2;
+const maxBet = 100;
+const minBet = 10;
+const cardValues = {
+    'Ace': 11,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'Jack': 10,
+    'Queen': 10,
+    'King': 10
+};
 
 /*---------- Variables (state) ---------*/
-
+let playerHand = [];
+let dealerHand = [];
+let playerScore = 0;
+let dealerScore = 0;
+let deck = [];
+let gameState = 'waiting';
+let currentBet = 0;
 
 /*----- Cached Element References  -----*/
 
